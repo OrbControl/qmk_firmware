4ea66d36760c9a9ed0c8f0d3f6319215397dfa9a
Kei <104486690+Orb-Control@users.noreply.github.com>
2022-12-03 23:02:17 -1000

    Changed PID again, and corrected numpad layout name

--- keyboards/cephalopod/numpad/keymaps/via/keymap.c
+++ keyboards/cephalopod/numpad/keymaps/via/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ ← │ ↓ │ → │ │   0   │ . │   │
      * └───┴───┴───┘ └───────┴───┴───┘
      */
-    [0] = LAYOUT(
+    [0] = LAYOUT_numpad(
         KC_PSCR,  KC_SCRL,  KC_MPLY,  KC_MUTE,  KC_VOLD, KC_VOLU,
         KC_INS,   KC_HOME,  KC_BSPC,  KC_PSLS,  KC_PAST, KC_PMNS,
         KC_DEL,   KC_PGUP,  KC_P7,    KC_P8,    KC_P9,   KC_PPLS,
